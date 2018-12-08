c0b16d38c8405e116a8b75cf19a8aeedfe6a8278
yongbinliang <2300855016@qq.com>
2018-12-08 16:22:57 +0800

    i am also gesty.this is my window pc computer

---
+++ cat/cat/spiders/cat_crawl.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from ..items import CatItem
+
+class CatCrawlSpider(scrapy.Spider):
+    name = 'cat_crawl'
+    allowed_domains = ['placekitten.com/g/']
+    base_url = 'http://placekitten.com/g/'
+    start_urls = ['http://placekitten.com/g//']
+
+    def parse(self, response):
+        link = response.xpath('//pre/a/@href').re(r'(\d.*)/')
+        url = []
+        for i in link:
+            url.append(self.base_url+i+'/'+i)
+        item = CatItem()
+        item['image_urls'] = url
+        yield item
