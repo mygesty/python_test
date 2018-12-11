e61f5e2e32e686c496e909d75eccaa1816cf0d5d
yongbinliang <2300855016@qq.com>
2018-12-11 22:24:30 +0800

    this is python program to crawl weibo users'messeges and it is distributed  on diffrent computer.

--- Weibo/weibo/items.py
+++ Weibo/weibo/items.py
@@ -3,54 +3,19 @@
 # Define here the models for your scraped items
 #
 # See documentation in:
-# http://doc.scrapy.org/en/latest/topics/items.html
+# https://doc.scrapy.org/en/latest/topics/items.html
 
-from scrapy import Item, Field
-
-
-class UserItem(Item):
-    collection = 'users'
-    
-    id = Field()
-    name = Field()
-    avatar = Field()
-    cover = Field()
-    gender = Field()
-    description = Field()
-    fans_count = Field()
-    follows_count = Field()
-    weibos_count = Field()
-    verified = Field()
-    verified_reason = Field()
-    verified_type = Field()
-    follows = Field()
-    fans = Field()
-    crawled_at = Field()
+import scrapy
 
 
-class UserRelationItem(Item):
-    collection = 'users'
-    
-    id = Field()
-    follows = Field()
-    fans = Field()
+class UserItem(scrapy.Item):
+    collection = 'messege'
+    follow_count = scrapy.Field()
+    fans_count = scrapy.Field()
+    statuses_count = scrapy.Field()
+    verified = scrapy.Field()
+    screen_name = scrapy.Field()
+    id = scrapy.Field()
 
-
-class WeiboItem(Item):
-    collection = 'weibos'
-    
-    id = Field()
-    attitudes_count = Field()
-    comments_count = Field()
-    reposts_count = Field()
-    picture = Field()
-    pictures = Field()
-    source = Field()
-    text = Field()
-    raw_text = Field()
-    thumbnail = Field()
-    user = Field()
-    created_at = Field()
-    crawled_at = Field()
-
-
+class FanItem(scrapy.Item):
+    fan_id = scrapy.Field()
